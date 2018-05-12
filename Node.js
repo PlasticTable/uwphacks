7ce2edc65b254a4edcad4ebd1c83410db83f7de8
Diana Tsai (天云) <didit7541@gmail.com>
2018-05-12 16:08:43 -0400

    Adding missing files

---
+++ Node.js
@@ -0,0 +1,20 @@
+Node.js
+
+What is node?
+// javascript is a really bad language but it's the only programming language that your browser will understand
+// develop servers with javascript using node.js
+
+// Languages made completely related to javascript and it's a server-side language
+// It's easy and it's modular
+// Great way to build modern websites using javascript on the client and server side
+
+// Node is single-threaded - can only run one thing at one time (one chain of execution you can't have multiple processes at a time)
+// Can be used for anything
+
+// Event driven javascript means something that happens that leads to something else
+
+
+
+request("link") is request to anything on the web such as API
+It is best to have callback functions 
+
